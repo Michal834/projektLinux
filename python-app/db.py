fd2da956773797e5bb5fbf26239c2b8c809e945a
Michal834 <michal.chwesiuk@o2.pl>
2025-11-14 09:57:47 +0100

    event

--- python-app/db.py
+++ python-app/db.py
@@ -3,19 +3,19 @@ class db:
     def __init__(self):
         self.connection = get_connection()
 
-    def get_users(self):
+    def get_events(self):
         conn = get_connection()
         cursor = conn.cursor(dictionary=True)
-        cursor.execute("SELECT * FROM users")
+        cursor.execute("SELECT * FROM wydarzenia")
         users = cursor.fetchall()
         cursor.close()
         conn.close()
         return users
     
-    def save_user(self, user_data):
+    def save_event(self, user_data):
         conn = get_connection()
         cursor = conn.cursor(dictionary=True)
-        cursor.execute("INSERT INTO users (name, email) VALUES (%s, %s)", (user_data['name'], user_data['email']))
+        cursor.execute("INSERT INTO wydarzenia (id_wydarzenia, nazwa, data_poczatek, data_koniec, opis, id_lokalizacja, id_kategoria, id_organizator) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (user_data['id_wydarzenia'], user_data['nazwa'], user_data['data_poczatek'], user_data['data_koniec'], user_data['opis'], user_data['id_lokalizacja'], user_data['id_kategoria'], user_data['id_organizator']))
         conn.commit()
         cursor.close()
         conn.close()
--- python-app/main.py
+++ python-app/main.py
@@ -4,16 +4,16 @@ from db import db
 
 app = FastAPI(debug=True)
 
-@app.get("/get-users")
+@app.get("/events")
 def get_users():
     my_db = db()
-    return my_db.get_users()
+    return my_db.get_events()
 
-@app.post("/save-user")
-async def save_user(request: Request):
+@app.post("/event")
+async def save_event(request: Request):
     user_data = await request.json()
     #sample payload: {"name":"Test","email":"moj@email"}
     my_db = db()
-    result = my_db.save_user(user_data)
+    result = my_db.save_event(user_data)
     return JSONResponse(status_code=201, content=result)
 
